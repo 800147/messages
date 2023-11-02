f7a1bab670d129c80d3ce7ca90b298de581c6006
800147 <800147@gmail.com>
2023-11-03 01:20:57 +0300

    Init time message

--- src/services/dbObject/dbObject.ts
+++ src/services/dbObject/dbObject.ts
@@ -5,7 +5,9 @@ declare global {
 }
 
 if (!global.dbObject) {
-  global.dbObject = { messages: ["first!"] };
+  global.dbObject = {
+    messages: [`First! Init time: ${new Date().toLocaleString()}`],
+  };
 }
 
 export const dbObject = global.dbObject;
